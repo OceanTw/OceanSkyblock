5dc0dbf474023a04561627d39827fdeb1f738782
Ocean <ocean@oce.lol>
2024-10-04 15:15:42 -0500

    idk

---
+++ api/src/main/java/lol/oce/api/Main.java
@@ -0,0 +1,7 @@
+package lol.oce.api;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ common/src/main/java/lol/oce/skyblock/Main.java
@@ -0,0 +1,7 @@
+package lol.oce.skyblock;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
